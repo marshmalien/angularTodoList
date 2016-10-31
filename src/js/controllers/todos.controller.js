d7fa25e810ecaee05947f549e51093b29882a477
Marliana Lara <marliana.lara@gmail.com>
2016-10-31 15:22:40 -0400

    add delete function

--- src/js/controllers/todos.controller.js
+++ src/js/controllers/todos.controller.js
@@ -1,11 +1,11 @@
 angular.module('todoList').controller('todoCtlr', function(todoStorage) {
   this.list = todoStorage.getData() || [];
   this.editing = false;
+  this.incomplete = "";
 
   this.updateItem = function() {
     todoStorage.setData(this.list);
     this.editing = false;
-    console.log('edit');
   };
 
   this.addNewItem = function() {
@@ -16,5 +16,10 @@ angular.module('todoList').controller('todoCtlr', function(todoStorage) {
     this.text = "";
     todoStorage.setData(this.list);
   };
+
+  this.deleteItem = function(id) {
+    this.list.splice(id, 1);
+    todoStorage.setData(this.list);
+  }
 
 });
