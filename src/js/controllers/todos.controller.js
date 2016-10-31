9567d2a0a6d8d55af3d2626b9c5c869f5646fcdf
Marliana Lara <marliana.lara@gmail.com>
2016-10-31 01:31:58 -0400

    toggle edit item

--- src/js/controllers/todos.controller.js
+++ src/js/controllers/todos.controller.js
@@ -1,3 +1,18 @@
 angular.module('todoList').controller('todoCtlr', function() {
 
+  this.list = [];
+
+
+  this.updateItem = function() {
+    console.log('item has changed');
+  };
+
+  this.addNewItem = function() {
+    this.list.push({
+      text: this.text,
+      completed: false
+    });
+    this.text = "";
+  };
+
 });
