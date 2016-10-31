94fb51a837735f96e796e886af2201c3dd9b6f8b
Marliana Lara <marliana.lara@gmail.com>
2016-10-30 22:44:25 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'lib/styles/main.min.css': 'src/styles/**/*.scss'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['src/styles/**/*'],
+                tasks: ['sass']
+            },
+
+            javascript: {
+                files: [
+                    'src/js/**/*'
+                ],
+                tasks: []
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', [
+        'sass',
+        'watch',
+    ]);
+};
---
+++ src/js/controllers/todos.controller.js
@@ -0,0 +1,3 @@
+angular.module('todoList').controller('todoCtlr', function() {
+
+});
